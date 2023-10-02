e6a603eeb5d8d4687ab361f93ade64a43f513d63
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-02 08:12:55 +0430

    add validation and password checking to the form

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { Title } from "./components/styles/FormTitle.styled";
 import { Label } from "./components/styles/Label.styled";
 import { Main } from "./components/styles/Main.styled";
 import { SubmitBtn } from "./components/styles/SubmitBtn.styled";
-
 import { useForm } from "react-hook-form";
-import { TermsOfService } from "./components/styles/TermsOfService";
 
 const App = () => {
-  const { handleSubmit, register, error } = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    getValues,
+    setError,
+    clearErrors,
+    trigger,
+  } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -18,15 +24,15 @@ const App = () => {
     <Main>
       <Title> Registration Form</Title>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <Label htmlFor="fullName">
+        <Label htmlFor="fullName" error={errors.fullName}>
           <input
             id="fullName"
             autoComplete="true"
             placeholder="Full Name"
-            {...register("fllName", { required: true, maxLength: 50 })}
+            {...register("fullName", { required: true, maxLength: 50 })}
           />
         </Label>
-        <Label htmlFor="userName">
+        <Label htmlFor="userName" error={errors.userName}>
           <input
             id="userName"
             autoComplete="true"
@@ -34,15 +40,15 @@ const App = () => {
             {...register("userName", { required: true, maxLength: 50 })}
           />
         </Label>
-        <Label htmlFor="userEmail">
+        <Label htmlFor="userEmail" error={errors.userEmail}>
           <input
             id="userEmail"
             placeholder="Email Address"
             type="email"
-            {...register("userEmail")}
+            {...register("userEmail", { required: true })}
           />
         </Label>
-        <Label htmlFor="password">
+        <Label htmlFor="password" error={errors.password}>
           <input
             id="password"
             type="password"
@@ -54,7 +60,7 @@ const App = () => {
             })}
           />
         </Label>
-        <Label htmlFor="confirmPassword">
+        <Label htmlFor="confirmPassword" error={errors.confirmPassword}>
           <input
             id="confirmPassword"
             type="password"
@@ -64,6 +70,16 @@ const App = () => {
               maxLength: 100,
               pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
             })}
+            onChange={(e) => {
+              let password = getValues("password");
+              let confPassword = e.target.value;
+
+              if (password !== confPassword) {
+                setError("confirmPassword");
+                trigger("confirmPassword");
+              }
+              clearErrors("confirmPassword");
+            }}
           />
         </Label>
         <SubmitBtn>Submit</SubmitBtn>
